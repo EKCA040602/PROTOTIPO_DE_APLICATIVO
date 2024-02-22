d0718cd318902d89477f8599179d5023ad61bdc3
EKCA040602 <esthivencruzayte837@gmail.com>
2024-02-22 17:01:05 -0500

    Atributos

--- Prototipo.py
+++ Prototipo.py
@@ -1,5 +1,10 @@
 print("Bienvenido a como hacer el prototipo")
 
-class Caracteristicas():
-    def __init__(self):
-        pass
+class CaracteristicasMineralogicas():
+    def __init__(self,color,peso_especifico,brillo,magnetismo,raya):
+        self.color=color
+        self.peso_especifica=peso_especifico
+        self.brillo=brillo
+        self.magnetismo=magnetismo
+        self.raya=raya
+        
